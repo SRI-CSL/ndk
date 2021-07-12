946bb0539325a874ae3a3a7cb9132756d81cd55c
Christopher Connolly <christopher.connolly@sri.com>
2021-07-12 12:49:48 -0700

    vsignal improvements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='ndk',
-      version='2.176',
+      version='2.178',
       description='Neurogram Deconvolution Kit',
       author='Chris Connolly',
       author_email='chris@neurome.com',
