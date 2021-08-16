7e4f036ab68673df05a104e17319d159a2bc2a85
Christopher Connolly <christopher.connolly@sri.com>
2021-08-16 09:03:27 -0700

    Sync

--- polar/devlist.py
+++ polar/devlist.py
@@ -9,11 +9,12 @@ async def run():
     devices = await BleakScanner.discover()
     all = devices
     for d in devices:
-        print(d)
+        if d.name[0:9] == 'Polar H10':
+            print(d.address, d.name, d.rssi)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
 
-
+# 293C33D4-2140-4D68-B85A-F5495CDA6D6C: Polar H10 78F5B92F
 # 61819F2C-AE8F-4F05-BB0F-E9A541D103A7: Polar H10 78F5B92F 78:F5:B9:2F
 # 61819F2C-AE8F-4F05-BB0F-E9A541D103A7: Polar H10 78F5B92F
