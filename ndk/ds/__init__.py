d8bb8b670043ac65921cbe9a6051b4f45bcc63d8
Chris Connolly <christopher.connolly@sri.com>
2025-09-19 18:03:27 -0700

    Mods to keep the game from breaking!

--- ndk/ds/__init__.py
+++ ndk/ds/__init__.py
@@ -1,4 +1,4 @@
-__all__ = [ 'dsfilter' 'wav', 'neo_in', 'edf_in', 'probe', 'cass', 'pre', 'mmap', 'nbf', 'ecg']
+__all__ = [ 'dsfilter' 'wav', 'neo_in', 'edf_in', 'probe', 'pre', 'mmap', 'nbf', 'ecg']
 
 # Ugh.  Python has some odd quirks...
 
@@ -7,7 +7,7 @@ from ndk.ds.neo_in import *
 from ndk.ds.nbf import *
 from ndk.ds.edf_in import *
 from ndk.ds.probe import *
-from ndk.ds.cass import *
+# from ndk.ds.cass import *
 from ndk.ds.pre import *
 from ndk.ds.mmap import *
 from ndk.ds.dsfilter import *
